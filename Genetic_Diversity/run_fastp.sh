0568ab82f4c54e142fa7cac4da99bff4c00f3c54
Milagros Guadalupe Rivera <97764650+Mrivera104@users.noreply.github.com>
2024-05-08 11:26:34 -0700

    Create run_fastp.sh

---
+++ Genetic_Diversity/run_fastp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Fastp script for my fastq file quality control 
+
+# Directory containing the input FASTQ files
+input_dir="/scratch1/migriver_CCGP/ncbi_dataset/omnic_data/"
+
+# Directory to store the output FASTP files
+output_dir="/scratch1/migriver_CCGP/ncbi_dataset/omnic_mapfiles"
+
+# Fastp settings
+
+fastp_options="--trim_poly_g --trim_poly_x -t 1 -T 1 --n_base_limit 5 --detect_adapter_for_pe --qualified_quality_phred 15 --unqualified_percent_limit 40 --cut_mean_quality 30 --average_qual 25 --low_complexity_filter --thread 8 --dont_overwrite --length_required 25 --correction --dedup --trim_poly_g -5 -3 -r -W 5"
+
+# Iterate over the input R1 files in the directory
+for input_file_1 in "${input_dir}"/*_1.fastq; do
+    # Extract the base name by removing "_1" and the extension
+    base_name=$(basename "${input_file_1}" | sed 's/_1.*//')
+
+    # Construct the paths for the input and output files
+    input_file_2="${input_dir}/${base_name}_2.fastq"
+    output_file1="${output_dir}/${base_name}.fastp.R1.fastq.gz"
+    output_file2="${output_dir}/${base_name}.fastp.R2.fastq.gz"
+    html_report="${output_dir}/${base_name}.fastp.html"
+    json_report="${output_dir}/${base_name}.fastp.json"
+
+    # Run fastp on the input files
+    fastp --in1 "${input_file_1}" --out1 "${output_file1}" \
+          --in2 "${input_file_2}" --out2 "${output_file2}" \
+          --html "${html_report}" \
+          --json "${json_report}" ${fastp_options}
+
+    echo "Processed: ${base_name}"
+done
